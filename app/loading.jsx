65848458e29cda5681d66a60292a4c0783939ab1
Ri0x2 <shabanabdallah309@gmail.com>
2024-10-16 14:14:24 +0300

    added cards

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-
 export default function loading() {
   return (
     <div className='flex justify-center mt-16'>
-        <img src="./spiner.svg" alt="loading...." />
+        <img src="/spiner.svg" alt="loading...." />
     </div>
   )
 }
